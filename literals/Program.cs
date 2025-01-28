af088a2e61a291d4e8ff7603c7baa56fdfa3a0ec
BuddhiEkanayake <buddhisri12@gmail.com>
2025-01-28 21:34:39 +0530

    Try C#

---
+++ literals/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("This is string");
+
+String firstName = "John";
+Console.WriteLine("First name is: " + firstName);
