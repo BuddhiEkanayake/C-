83a17277f435a19ec73c2ad1110795d9da6dbe16
BuddhiEkanayake <buddhisri12@gmail.com>
2025-01-30 13:43:36 +0530

    Strings

---
+++ literals/Variabls/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+		int num1 = 10;
+		int num2 = 78;
+		int sum = num1 + num2;
+
+
+        Console.WriteLine(@"location=C:\User|Desktop");
+        
+//String Interpolation
+
+        string name="Kamal";
+
+        Console.WriteLine($"my name is {name}");
+	
+
