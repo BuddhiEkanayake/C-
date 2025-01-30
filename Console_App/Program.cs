6d3e2b747aed080c464c43d7e5b3c880b23e0097
BuddhiEkanayake <buddhisri12@gmail.com>
2025-01-30 22:16:30 +0530

    Simple Conditions

---
+++ Console_App/Program.cs
@@ -0,0 +1,31 @@
+﻿var dice=new Random();
+
+var roll1 = dice.Next(1,7);
+var roll2 = dice.Next(1,7);
+var roll3 = dice.Next(1,7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Value is {roll1} + {roll2} + {roll3} = {total}");
+
+//total is greater than 14 yo won
+//total is less than 14 you loose
+
+if((roll1==roll2) || (roll2==roll3) || (roll3==roll1))
+{
+    Console.WriteLine("Add extra 2 marks in total value");
+    total+=2;
+}
+
+Console.WriteLine($"New  total value = {total}");
+
+if(total>14)
+{
+    Console.WriteLine("You won");
+}
+
+else
+{
+    Console.WriteLine("You loss");
+}
+
