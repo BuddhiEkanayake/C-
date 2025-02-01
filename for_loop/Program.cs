75b649d445a27fbb7e579235bde6384601ecf971
BuddhiEkanayake <buddhisri12@gmail.com>
2025-02-01 13:28:29 +0530

    for_loop

---
+++ for_loop/Program.cs
@@ -0,0 +1,23 @@
+﻿//for(<variable>, <condition> ,<iteration>;){}
+
+/*for(int i=0; i<=10; i++)
+{
+    Console.WriteLine(i);
+}
+*/
+
+for(int i =0; i<=100; i++)
+{
+    if(i%3==0)
+    {
+        Console.WriteLine($"{i}= Bizz");
+    }
+
+    if((i%3==0) && (i%5==0))
+    {
+        Console.WriteLine ($"{i}=Bizz Puzzl");
+    }
+
+    else
+    {Console.WriteLine(i);}
+}
