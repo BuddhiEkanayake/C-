499a628e1233b2c4fddb858157ebc1cff31a220e
BuddhiEkanayake <buddhisri12@gmail.com>
2025-02-10 16:55:22 +0530

    Swich_Case

---
+++ Swich_Case/Program.cs
@@ -0,0 +1,20 @@
+﻿DisplayMeasurement(10.0);
+DisplayMeasurement(0.0);
+DisplayMeasurement(20.0);
+
+
+void DisplayMeasurement(double measurement)
+{
+    switch (measurement)
+    {
+        case<0.0:
+            Console.WriteLine($"Measured valid is {measurement} is too low");
+            break;
+        case>15.0:
+            Console.WriteLine($"Measured valid is {measurement} is high");
+            break;
+        default:
+            Console.WriteLine($"Measured valid is {measurement} is normal");
+            break;
+    }
+}
