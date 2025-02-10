bde7677b64360f10fbf0e961660f3472d6f2037d
BuddhiEkanayake <buddhisri12@gmail.com>
2025-02-10 12:16:43 +0530

    While Loop & do-While loop

--- do_while_loop/Program.cs
+++ do_while_loop/Program.cs
@@ -1,10 +1,34 @@
-﻿int i=0;
+﻿/*int i=0;
  do{
+   //always execute first statement
     Console.WriteLine(i);
     i++;
  }
 
- while(i<10);
+ while(i<10);*/
+//---------------------------------------------------------
+  Random amount = new Random();
 
+  int current=0;
+  int index=1;
 
- 
+   do{
+      current =amount.Next(1,10);
+      Console.WriteLine($"{index}-{current}");
+      index++;
+   } while(current!=7);
+   
+//----------------------while loop-------------------------
+
+ Random number= new Random();
+  
+  int newNumber= number.Next(1,11);
+  Console.WriteLine($"First Number is= {newNumber}");
+
+  while(newNumber>= 3)
+  {
+    Console.WriteLine(newNumber);
+    newNumber= number.Next(1,11);
+  }
+
+  Console.WriteLine($"Last number is= {newNumber}");
