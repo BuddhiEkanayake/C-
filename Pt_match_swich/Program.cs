0b2a84c6fb161834328b60e7df4536e850241a1c
BuddhiEkanayake <buddhisri12@gmail.com>
2025-02-11 15:21:30 +0530

    Value patten

---
+++ Pt_match_swich/Program.cs
@@ -0,0 +1,18 @@
+﻿// patten matching with C# swich expressions
+
+//Value pattern
+
+string color = "g";
+
+var result= color switch 
+{
+    "r" => "color is red",
+    "g" => "color is green",
+    "b" => "color is blue",
+    _ => "unknown color"
+};
+
+Console.WriteLine(result);
+
+
+    
