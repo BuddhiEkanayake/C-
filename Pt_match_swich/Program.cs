d6784a03f4081794631fac6beea548274216f7a4
BuddhiEkanayake <buddhisri12@gmail.com>
2025-02-11 21:17:49 +0530

    Switch-Type Patten

--- Pt_match_swich/Program.cs
+++ Pt_match_swich/Program.cs
@@ -14,5 +14,27 @@ var result= color switch
 
 Console.WriteLine(result);
 
+//Type pattern
 
+int age=20;
+string name="John";
+List<string> colors = new List<string> { "red", "green", "blue" };
+var numbers= new int[] { 1, 2, 3, 4, 5 };
+double salary= 1000.00;
+
+
+object CheckType(object val) => val switch
+{
+    int => "integer Type",
+    string => "string Type",
+    List<string> => "List of string Type",
+    Array => "Array Type",
+    _ => "unknown Type"
+};
+
+Console.WriteLine(CheckType(age));
+Console.WriteLine(CheckType(name));
+Console.WriteLine(CheckType(colors));
+Console.WriteLine(CheckType(numbers));
+Console.WriteLine(CheckType(salary));
     
