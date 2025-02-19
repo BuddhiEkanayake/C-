977592e4cda72f76cf11affa3bcdb2b7c04dee6e
BuddhiEkanayake <buddhisri12@gmail.com>
2025-02-19 11:24:19 +0530

    OOP part in C#

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -5,22 +5,75 @@ namespace CSharpOOP;
 
 public class BankAccount
 {
+
+    private static int accountNumberSeed = 1234567890;
+    private List<Transaction> allTransactions = new List<Transaction>();
+
+
+
     public string? Numbers { get;}
     public string? Owner { get; set;}
-    public decimal Balance { get;}
+    public decimal Balance 
+    {
+         get
+         {
+            decimal balance = 0;
+            foreach (var item in allTransactions)
+            {
+                balance += item.Amount;
+            }
+            return balance;
+         }
+         
+         
+    }
 
     // A constructor is a special method that is called when an instance of a class is created.
 
-    public BankAccount()
+    public BankAccount(string name, decimal initialBalance)
     {
-        Console.WriteLine("Constructor called");
+        this.Owner = name; // this keyword is used to refer to the current instance of the class.
+        
+        MakeDeposit(initialBalance, DateTime.Now, note: "Initial balance");
+
+        this.Numbers = accountNumberSeed.ToString();
+        accountNumberSeed++;
+
+
     }
 
     public void MakeDeposit(decimal amount, DateTime date, string note)
-    {}
+    {
+        if(amount<=0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount),
+            message:"Amount of deposit must be positive");
+        }
+
+        var deposit = new Transaction(amount,date,note);
+
+        allTransactions.Add(deposit);
+
+    }
 
     public void MakeWithdrawal(decimal amount, DateTime date, string note)
-    {}
+    {
+
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount),
+            message: "Amount of withdrawal must be positive");
+        }
+
+        if (Balance - amount < 0)
+        {
+            throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+        }
+
+
+        var withdrawal = new Transaction(-amount,date,note);
+        allTransactions.Add(withdrawal);
+    }
 
 
 }
--- OOP/Program1.cs
+++ OOP/Program1.cs
@@ -1,3 +1,12 @@
 using CSharpOOP;
 
-BankAccount account = new BankAccount();
+BankAccount account = new BankAccount(name: "John Doe", initialBalance: 1000);
+
+Console.WriteLine(account.Owner);
+Console.WriteLine(account.Balance);
+
+Console.WriteLine($"Account owner is-{account.Owner} and account number is-{account.Numbers}");
+
+BankAccount account2 = new BankAccount(name:"Nirmali Herath", initialBalance: 2000);
+
+Console.WriteLine($"Account Owner is-{account2.Owner} and account number is-{account2.Numbers}");
