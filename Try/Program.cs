76361acab715bcce5543dba2e5506813f025d220
BuddhiEkanayake <buddhisri12@gmail.com>
2025-03-01 00:31:07 +0530

    Remember past things

---
+++ Try/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int marks;
+Console.WriteLine("Enter your marks:");
+
+marks = Convert.ToInt32(Console.ReadLine());
+ if(marks<35){
+    Console.WriteLine("You have failed");
+ }
+else if(marks>=35 && marks<50){
+        Console.WriteLine("You have passed");
+}
+else if (marks>= 50 && marks<65){
+    Console.WriteLine("You have passed with 2nd class");
+}
+else{
+    Console.WriteLine("You have passed with 1st class");
+}
